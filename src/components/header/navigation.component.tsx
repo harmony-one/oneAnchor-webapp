4dd5b03d2e203f3d924972af58c4a24b4ba07223
fegloff <fegloff@gmail.com>
2022-04-29 00:02:09 -0500

    remove warning on navigation component

--- src/components/header/navigation.component.tsx
+++ src/components/header/navigation.component.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import "./navigation.styles.scss";
 
 const Navigation: React.FC = () => {
